186577b04a749021133f95c7e0e7e41cae089584
xjvalgar786-lab <xjvalgar786@ieshnosmachado.org>
2026-05-18 20:59:04 +0200

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -102,7 +102,7 @@ class AuthController {
       // Configurar cookie HttpOnly
       res.cookie('token', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
+        secure: true,
         sameSite: "none",
         maxAge: 24 * 60 * 60 * 1000 // 24 horas
       });
