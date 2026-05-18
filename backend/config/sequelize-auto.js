d636970d7ea3ace4558c8f0617511f8e94c3be35
xjvalgar786-lab <xjvalgar786@ieshnosmachado.org>
2026-05-18 20:32:19 +0200

    Update sequelize-auto.js

--- backend/config/sequelize-auto.js
+++ backend/config/sequelize-auto.js
@@ -2,21 +2,15 @@ const SequelizeAuto = require("sequelize-auto");
 const config = require('./config');
 
 const auto = new SequelizeAuto(
-  config.db.name, 
-  config.db.user, 
-  config.db.password, 
+  process.env.DATABASE_URL,
   {
-    host: config.db.host,
-    port: config.db.port,
     dialect: "mysql",
     directory: "./models",
-    caseModel: 'c', 
-    caseFile: "c", 
+    caseModel: "c",
+    caseFile: "c",
     additional: {
       timestamps: false,
-      
     },
-    
   }
 );
 
