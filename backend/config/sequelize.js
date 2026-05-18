5fc3a025a51bbf1b56dd7664cfe9383ec6c3d060
xjvalgar786-lab <xjvalgar786@ieshnosmachado.org>
2026-05-18 20:22:45 +0200

    cors
    a

--- backend/config/sequelize.js
+++ backend/config/sequelize.js
@@ -3,18 +3,10 @@ const { Sequelize } = require("sequelize");
 const config = require("./config");
 
 // Instanciar sequelize  para conectar a mysql
-const sequelize = new Sequelize(
-  config.db.name, // nombre bd
-  config.db.user, // usuario
-  config.db.password, // password
-  {
-    // objeto con opciones de conexion
-    host: config.db.host, // Cambia esto por la dirección del servidor MySQL
-    port: config.db.port, // Cambia esto por el puerto del servidor MySql
-    dialect: "mysql", // Especificar el dialecto de la base de datos
-    logging: false, // Desactiva el logging de las consultas SQL
-  }
-);
+const sequelize = new Sequelize(process.env.MYSQL_URL, {
+  dialect: "mysql",
+  logging: false,
+});
 
 // Probar la conexión
 (async () => {
