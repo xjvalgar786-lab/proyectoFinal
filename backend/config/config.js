323dcaf08a68fb60eb6ffd2b7ff63e785aee0751
xjvalgar786-lab <xjvalgar786@ieshnosmachado.org>
2026-05-29 09:25:29 +0200

    Update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,4 +1,6 @@
-require("dotenv").config();
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV || "development"}`,
+});
 
 module.exports = {
   port: process.env.PORT || 5000,
@@ -9,7 +11,12 @@ module.exports = {
     name: process.env.DB_NAME || process.env.MYSQLDATABASE || process.env.MYSQL_DATABASE,
     port: process.env.DB_PORT || process.env.MYSQLPORT,
   },
-  secretKey: process.env.JWT_SECRET,
+  secretKey: process.env.JWT_SECRET || "default_secret",
 };
 
+console.log("DBNAME:",process.env.DB_NAME);
+console.log("DBHOST:",process.env.DB_HOST);
+console.log("DBUSER:",process.env.DB_USER);
+console.log("DBPORT:",process.env.DB_PORT);
+console.log("NODE_ENV:",process.env.NODE_ENV);
 console.log("process.env.JWT_SECRET:",process.env.JWT_SECRET);
